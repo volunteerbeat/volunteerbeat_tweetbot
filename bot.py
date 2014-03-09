93edbcb9f4ca95948833e34adc890da89c611ecc
Charles Lee <chug2k@gmail.com>
2014-03-09 00:20:26 -0800

    i need syntax help

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ class TwitterAPI:
         """Send a tweet"""
         self.api.update_status(message)
 
-    def mentions:
+    def mentions():
         return self.api.mentions()
 
 if __name__ == "__main__":
@@ -43,7 +43,6 @@ if __name__ == "__main__":
     db = mongo_conn().app22869812
     while True:
         # Get tweets here
-        seenTweets = db.seenTweets.find({})
         for tweet in twitter.mentions():
             if db.seenTweets.find({tweetId: tweet.id}) is None:
                 twitter.tweet("Great! Create task for {0}:{1}".format(tweet.author.screen_name, tweet.entries['hashtags']));
